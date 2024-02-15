3269d5fb6331522509bc652afe9ea03d7d62f3b1
kareem <kareemesam52@yahoo.com>
2024-02-15 06:29:41 +0200

    ContainDuplicates2 problem

---
+++ src/arrays/containsDuplicates2.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.HashMap;
+
+public class containsDuplicates2 {
+    public static void main(String[] args) {
+
+        System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2));
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(nums[i])) {
+                if (Math.abs(i - hash.get(nums[i])) <= k) {
+                    return true;
+                }
+            }
+            hash.put(nums[i], i);
+        }
+        return false;
+    }
+}
+
+/**
+ * Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ */
+
