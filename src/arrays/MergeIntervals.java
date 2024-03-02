bbbe595eb12dc9d072875db0196cbc9ef9fe5c7d
kareem <kareemesam52@yahoo.com>
2024-03-02 18:57:22 +0200

    solve MergeIntervals problem

--- src/arrays/MergeIntervals.java
+++ src/arrays/MergeIntervals.java
@@ -2,16 +2,40 @@ package arrays;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class MergeIntervals {
     public static void main(String[] args) {
-        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 5}, {0, 4}, {3, 10}, {2, 5}})));
+        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 4}, {4, 5}})));
     }
 
     public static int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(c -> c[0]));
+        List<int[]> result = new java.util.ArrayList<>();
+        result.add(intervals[0]);
         for (int[] interval : intervals) {
+            if (interval[0] <= result.getLast()[1]) {
+                result.getLast()[1] = Math.max(interval[1], result.getLast()[1]);
+            } else {
+                result.add(interval);
+            }
         }
-        return intervals;
+        return result.toArray(new int[result.size()][]);
     }
 }
+/**
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+ * Example 2:
+ * <p>
+ * Input: intervals = [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ */
