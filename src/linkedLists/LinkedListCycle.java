12710e9770e97a372d5092a96ea180ea4cf8d1f5
kareem <kareemesam52@yahoo.com>
2024-03-05 00:47:35 +0200

    LinkedListCycle problem

---
+++ src/linkedLists/LinkedListCycle.java
@@ -0,0 +1,28 @@
+package linkedLists;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+
+    }
+}
